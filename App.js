fa2770e8ac7117580c87627a45a4eed0d5bd30e1
ledda <robert.ledang@stud.ntnu.no>
2018-10-06 22:01:41 +0200

    #4 Added NativeBase and implemented static layout of list view

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ListView from './components/ListView';
 
 export default class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = { loading: true };
+    }
+
+    async componentWillMount() {
+      await Expo.Font.loadAsync({
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+        Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+      });
+      this.setState({ loading: false });
+    }
+
   render() {
+    if (this.state.loading) {
+      return <Expo.AppLoading />;
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ListView />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/ListView.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Card, CardItem, Body, Text, Footer, Button } from 'native-base';
+export default class ListView extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Card>
+            <CardItem header bordered>
+              <Text>Overskrift oppgave 1</Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                   Tekst
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem footer>
+              <Text>Du har igjen 36 av 4000 skritt</Text>
+            </CardItem>
+          </Card>
+
+          <Card>
+            <CardItem header bordered>
+              <Text>Overskrift oppgave 2</Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                   Tekst
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem footer>
+              <Text>Du har igjen 355 av 4500 skritt</Text>
+            </CardItem>
+          </Card>
+
+          <Card>
+            <CardItem header bordered>
+              <Text>Overskrift oppgave 3</Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                   Tekst
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem footer>
+              <Text>Du har igjen 370 av 3000 skritt</Text>
+            </CardItem>
+          </Card>
+
+          <Card>
+            <CardItem header bordered>
+              <Text>Overskrift oppgave 4</Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                   Tekst
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem footer>
+              <Text>Du har igjen 1800 av 2000 skritt</Text>
+            </CardItem>
+          </Card>
+
+          <Card>
+            <CardItem header bordered>
+              <Text>Overskrift oppgave 5</Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                   Tekst
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem footer>
+              <Text>Du har igjen 4609 av 10000 skritt</Text>
+            </CardItem>
+          </Card>
+        </Content>
+        <Footer>
+          <Button transparent light>
+            <Text>Opprett ny oppgave</Text>
+          </Button>
+        </Footer>
+      </Container>
+    );
+  }
+}
