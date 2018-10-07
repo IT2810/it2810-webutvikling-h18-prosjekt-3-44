4420c0ad63ccf1f48f9c392c64c073a527805a19
Kenneth Juul Wannebo <kenneth@resight.no>
2018-10-07 21:52:16 +0200

    #9 Basic asyncstorage-setup. Load Items from storage + linting setup

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import ListView from './components/ListView';
 import NewItem from './components/NewItem';
@@ -17,18 +17,54 @@ const RootStack = createStackNavigator(
 
 export default class App extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = { loading: true };
-    }
-
-    async componentWillMount() {
-      await Expo.Font.loadAsync({
-        Roboto: require("native-base/Fonts/Roboto.ttf"),
-        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-        Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+    super(props);
+    this.state = { loading: true };
+    this.item = {
+      id: "1",
+      header: "En viktig oppgave",
+      task: "Dette må gjøres!",
+      stepsTaken: 0,
+      stepsGoal: 400,
+      startDate: new Date(),
+    };
+    this._storeItem().then(() => {
+      this._retrieveItem().then((item) => {
+        console.log("First test: " + JSON.stringify(item));
+      }).catch((error) => {
+        console.log("Item retrieve error: " + error);
       });
-      this.setState({ loading: false });
-    }
+    }).catch((error) => {
+      console.log("Item store error: " + error);
+    });
+  }
+
+	_storeItem = async () => {
+		try {
+			await AsyncStorage.setItem(this.item.id, JSON.stringify(this.item));
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	_retrieveItem = async () => {
+		try {
+			const value = await AsyncStorage.getItem(this.item.id);
+			return JSON.parse(value);
+		} catch (error) {
+			console.log(error.message);
+		}
+		return
+	}
+		
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+    });
+    this.setState({ loading: false });
+  }
 
   render() {
     if (this.state.loading) {
