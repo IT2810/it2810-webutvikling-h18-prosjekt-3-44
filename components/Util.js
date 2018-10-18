eaf38d23acdcdd7e25200b5edef58c69deb61833
ledda <robert.ledang@stud.ntnu.no>
2018-10-18 23:52:40 +0200

    #23 Created util class

---
+++ components/Util.js
@@ -0,0 +1,10 @@
+
+export function stepsNotEqual(stepTaken, goal) {
+  if(this.props.item.stepTaken !== this.props.item.stepGoal) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+this.props.item.stepTaken !== this.props.item.stepGoal
